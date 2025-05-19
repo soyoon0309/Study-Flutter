67ae6119a7e2ed014c8dfc45414ab032572a1e65
soyoon0309 <soyoon0309@naver.com>
2025-05-19 19:04:52 +0900

    list_screen 구현
    list_screen에서 detail_screen으로 데이터를 전달할 수 있도록 BookTile 위젯을 새롭게 생성하고 해당 위젯에 데이터를 추가함
    list_screen에서는 보이지 않지만 이후 detail_screen에서 사용할 예정

---
+++ flutter_book_list/lib/screens/detail_screen.dart
@@ -0,0 +1,16 @@
+//lib/screens/detail_screen.dart
+//세부 사항 출력 창
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('책 제목'),
+      ),
+      body: Container(),
+    );
+  }
+}
