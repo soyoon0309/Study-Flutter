bdf66a6610d55692af17ad6464f2d0b77ce1c169
soyoon0309 <soyoon0309@naver.com>
2025-05-25 02:12:49 +0900

    4.5 타이머 기능 구현

--- flutter_timer/lib/screens/timer_screen.dart
+++ flutter_timer/lib/screens/timer_screen.dart
@@ -1,5 +1,10 @@
 //lib/screens/timer_screen.dart
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:sprintf/sprintf.dart';
+
+enum TimerStatus {running, paused, stopped, resting}
 
 class TimerScreen extends StatefulWidget{
   @override
@@ -7,19 +12,111 @@ class TimerScreen extends StatefulWidget{
 }
 
 class _TimerScreenState extends State<TimerScreen>{
+  static const WORK_SECONDS = 25;
+  static const REST_SECONDS = 5;
+
+  late TimerStatus _timerStatus;
+  late int _timer;
+  late int _pomodoroCount;
+
+  @override
+  void initState(){
+    super.initState();
+    _timerStatus = TimerStatus.stopped;
+    print(_timerStatus.toString());
+    _timer = WORK_SECONDS;
+    _pomodoroCount = 0;
+  }
+
+  String secondsToString(int seconds){
+    return sprintf("%02d:%02d", [seconds ~/ 60, seconds % 60]);
+  }
+
+  void run() {
+    setState(() {
+      _timerStatus = TimerStatus.running;
+      print("[=>] "+_timerStatus.toString());
+      runTimer();
+    });
+  }
+
+  void rest() {
+    setState(() {
+      _timer = REST_SECONDS;
+      _timerStatus = TimerStatus.resting;
+      print("[=>] "+_timerStatus.toString());
+    });
+  }
+
+  void pause() {
+    setState(() {
+      _timerStatus = TimerStatus.paused;
+      print("[=>] "+_timerStatus.toString());
+    });
+  }
+
+  void resume() {
+    run();
+  }
+
+  void stop() {
+    setState(() {
+      _timer = WORK_SECONDS;
+      _timerStatus = TimerStatus.stopped;
+      print("[=>] "+_timerStatus.toString());
+    });
+  }
+
+  void runTimer() async {
+    Timer.periodic(Duration(seconds: 1), (Timer t) {
+      switch (_timerStatus) {
+        case TimerStatus.paused:
+          t.cancel();
+          break;
+        case TimerStatus.stopped:
+          t.cancel();
+          break;
+        case TimerStatus.running:
+          if(_timer <= 0) {
+            print("작업 완료!");
+            rest();
+          } else {
+            setState(() {
+              _timer -= 1;
+            });
+          }
+          break;
+        case TimerStatus.resting:
+          if (_timer <= 0) {
+            setState(() {
+              _pomodoroCount += 1;
+            });
+            print("오늘 $_pomodoroCount개의 뽀모도로를 달성했습니다.");
+            t.cancel();
+            stop();
+          } else {
+            setState(() {
+              _timer -= 1;
+            });
+          }
+          break;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final List<Widget> _runningButtons = [
       ElevatedButton(
-        onPressed: () {},
+        onPressed: _timerStatus == TimerStatus.paused ? resume : pause,
         style: ElevatedButton.styleFrom(backgroundColor: Colors.blueAccent), 
         child: Text(
-          1 == 2 ? '계속하기' : '일시정지', //일시정지 중? 계속하기 : 일시정지 
+          _timerStatus == TimerStatus.paused ? '계속하기' : '일시정지', //일시정지 중? 계속하기 : 일시정지 
           style: TextStyle(color: Colors.white, fontSize: 16)),
       ),
       Padding(padding: EdgeInsets.all(20)),
       ElevatedButton(
-        onPressed: () {}, 
+        onPressed: stop, 
         style: ElevatedButton.styleFrom(backgroundColor: Colors.grey),
         child: Text(
           '포기하기',
@@ -28,8 +125,8 @@ class _TimerScreenState extends State<TimerScreen>{
       ),
     ];
     final List<Widget> _stoppedButtons = [
-      ElevatedButton(onPressed: () {}, 
-      style: ElevatedButton.styleFrom(backgroundColor: 1 == 2 ? Colors.lightGreen : Colors.blueAccent), //휴식중? green : blue
+      ElevatedButton(onPressed: run, 
+      style: ElevatedButton.styleFrom(backgroundColor: _timerStatus == TimerStatus.resting ? Colors.lightGreen : Colors.blueAccent), //휴식중? green : blue
       child: Text(
         '시작하기',
         style: TextStyle(color: Colors.white,
@@ -48,13 +145,13 @@ class _TimerScreenState extends State<TimerScreen>{
             Container(
               decoration: BoxDecoration(
                 shape: BoxShape.circle,
-                color: 1 == 2? Colors.lightGreen : Colors.blueAccent,
+                color: _timerStatus == TimerStatus.resting? Colors.lightGreen : Colors.blueAccent,
               ),
               height: MediaQuery.of(context).size.height * 0.5,
               width: MediaQuery.of(context).size.width * 0.6,
               child: Center(
                 child: Text(
-                  '00:00',
+                  secondsToString(_timer),
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 48,
@@ -65,9 +162,9 @@ class _TimerScreenState extends State<TimerScreen>{
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: 1 == 2  //휴식중? 버튼 있 / 없 ,
+              children: _timerStatus == TimerStatus.resting  //휴식중? 버튼 있 / 없 ,
                   ? const []
-                  : 1 == 2 //정지? 정지중 버튼 : 작업중 버튼 
+                  : _timerStatus == TimerStatus.stopped //정지? 정지중 버튼 : 작업중 버튼 
                       ? _stoppedButtons
                       : _runningButtons,
             )
