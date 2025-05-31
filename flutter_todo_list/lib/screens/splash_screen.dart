2c749fb077979f98a4c859219924abea9390db42
soyoon0309 <soyoon0309@naver.com>
2025-06-01 00:03:04 +0900

    SharedPreferences ing
    moveScreen() 작성 중 중단
    await부터 작성 요망

---
+++ flutter_todo_list/lib/screens/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  void moveScreen() async {
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      moveScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('SplashScreen', style: TextStyle(fontSize: 20)),
+            Text('나만의 일정 관리 : TODO List App', style: TextStyle(fontSize: 20)),
+          ],
+        ),
+      ),
+    );
+  }
+}
